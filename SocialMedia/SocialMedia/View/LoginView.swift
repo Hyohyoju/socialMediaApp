948a728134eb43d07decad8f1cc389ffa0df50f1
이효주 <julie08051@naver.com>
2024-12-24 17:18:36 +0900

    Feat: 로그인 화면, 등록 화면, 프로필 사진 클릭 시 변경 가능

---
+++ SocialMedia/SocialMedia/View/LoginView.swift
@@ -0,0 +1,246 @@
+//
+//  LoginView.swift
+//  SocialMedia
+//
+//  Created by 이효주 on 12/24/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct LoginView: View {
+    // MARK: - USER DETAILS
+    @State var emailID: String = ""
+    @State var password: String = ""
+    // MARK: - View Properties
+    @State var createAccount: Bool = false
+    
+    // MARK: - Body
+    var body: some View {
+        VStack {
+            Text("Lets Sign you in")
+                .font(.largeTitle.bold())
+                .hAlign(.leading)
+            
+            Text("Welcome Back,\nYou have been missed")
+                .font(.title3)
+                .hAlign(.leading)
+                .padding(.top, 3)
+            
+            VStack(spacing: 12) {
+                TextField("Email", text: $emailID)
+                    .textContentType(.emailAddress)
+                    .border(1, .gray.opacity(0.5))
+                    .padding(.top, 25)
+                
+                SecureField("Password", text: $password)
+                    .textContentType(.emailAddress)
+                    .border(1, .gray.opacity(0.5))
+                
+                Button("Reset password?", action: {})
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .tint(.black)
+                    .hAlign(.trailing)
+                
+                Button {
+                    
+                } label: {
+                    // MARK: - LOGIN BUTTON
+                    Text("Sign in")
+                        .foregroundColor(.white)
+                        .hAlign(.center)
+                        .fillView(.black)
+                }
+                .padding(.top, 10)
+            }
+            
+            // MARK: - REGISTER BUTTON
+            HStack {
+                Text("Don't have an account?")
+                    .foregroundColor(.gray)
+                
+                Button("Register Now") {
+                    createAccount.toggle()
+                }
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            }
+            .font(.callout)
+            .vAlign(.bottom)
+        }
+        .vAlign(.top)
+        .padding(15)
+        // MARK: sheet를 통한 Register View
+        .fullScreenCover(isPresented: $createAccount) {
+            RegisterView()
+        }
+    }
+}
+
+
+// MARK: REGISTER VIEW
+struct RegisterView: View {
+    // MARK: - USER DETAILS
+    @State var emailID: String = ""
+    @State var password: String = ""
+    @State var userName: String = ""
+    @State var userBio: String = ""
+    @State var userBioLink: String = ""
+    @State var userProfilePicData: Data?
+    
+    // MARK: - View Properties
+    @Environment(\.dismiss) var dismiss
+    @State var showImagePicker: Bool = false
+    @State var photoItem: PhotosPickerItem?
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack {
+            Text("Lets Register\nAccount")
+                .font(.largeTitle.bold())
+                .hAlign(.leading)
+            
+            Text("Hello user, have a wonderful journey")
+                .font(.title3)
+                .hAlign(.leading)
+                .padding(.top, 3)
+            
+            // MARK: - 작은 사이즈를 위해서
+            ViewThatFits {
+                ScrollView(.vertical, showsIndicators: false) {
+                    HelperView()
+                }
+                HelperView()
+            }
+            
+            // MARK: - REGISTER BUTTON
+            HStack {
+                Text("Already Have an account?")
+                    .foregroundColor(.gray)
+                
+                Button("Login Now") {
+                    dismiss()
+                }
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+            }
+            .font(.callout)
+            .vAlign(.bottom)
+        }
+        .vAlign(.top)
+        .padding(15)
+        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+        .onChange(of: photoItem) { newValue in
+            // MARK: Extracting UIImage From PhotoItem
+            if let newValue {
+                Task {
+                    do {
+                        guard let imageData = try await newValue.loadTransferable(type: Data.self) else {return}
+                        // MARK: - UI는 반드시 메인 스레드에 업데이트되어야 한다.
+                        await MainActor.run(body: {
+                            userProfilePicData = imageData
+                        })
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func HelperView() -> some View {
+        VStack(spacing: 12) {
+            ZStack {
+                if let userProfilePicData, let image = UIImage(data: userProfilePicData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } else {
+                    Image("NullProfile")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.top, 3)
+                }
+            }
+            .frame(width: 85, height: 85)
+            .clipShape(Circle())
+            .contentShape(Circle())
+            .onTapGesture {
+                showImagePicker.toggle()
+            }
+            .padding(.top, 25)
+            
+            TextField("Username", text: $userName)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+                .padding(.top, 3)
+            
+            TextField("Email", text: $emailID)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            SecureField("Password", text: $password)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextField("About You", text: $userBio)
+                .frame(minHeight: 100, alignment: .top)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            TextField("Bio link (Optional)", text: $userBioLink)
+                .textContentType(.emailAddress)
+                .border(1, .gray.opacity(0.5))
+            
+            Button {
+                
+            } label: {
+                // MARK: - LOGIN BUTTON
+                Text("Sign up")
+                    .foregroundColor(.white)
+                    .hAlign(.center)
+                    .fillView(.black)
+            }
+            .padding(.top, 10)
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
+
+// MARK: - VIEW EXTENSION FOR UI BUILIDING
+extension View {
+    func hAlign(_ alignment: Alignment) -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: alignment)
+    }
+    
+    func vAlign(_ alignment: Alignment) -> some View {
+        self
+            .frame(maxHeight: .infinity, alignment: alignment)
+    }
+    
+    // MARK: Custom Border View With Padding
+    func border(_ width: CGFloat, _ color: Color) -> some View {
+        self.padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background {
+                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                    .stroke(color, lineWidth: width)
+            }
+    }
+    
+    // MARK: Custom Fill View With Padding
+    func fillView(_ color: Color) -> some View {
+        self.padding(.horizontal, 15)
+            .padding(.vertical, 10)
+            .background {
+                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                    .fill(color)
+            }
+    }
+}

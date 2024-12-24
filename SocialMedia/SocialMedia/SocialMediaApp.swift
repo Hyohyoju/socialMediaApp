62f32137813ad29d74bff38574f8c5cfa1ced161
Lee Hyoju <168440628+Hyohyoju@users.noreply.github.com>
2024-12-24 15:14:21 +0900

    기본 세팅

---
+++ SocialMedia/SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by 이효주 on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
